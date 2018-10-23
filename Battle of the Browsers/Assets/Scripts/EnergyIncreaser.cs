cc644eea1d721c4857dece7c7f4c5e71b3c58ffd
Jackson Tix <jbtix22@blakeschool.org>
2018-10-23 13:51:44 -0500

    Added Bar - No Code Yet

---
+++ Battle of the Browsers/Assets/Scripts/EnergyIncreaser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyIncreaser : MonoBehaviour
+{
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += new Vector3(0, 0.1F, 0);
+    }
+}
