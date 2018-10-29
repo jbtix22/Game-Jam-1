5211a71359344bb54f3a2380416f35ab4126c911
Sam Hazlett22 <snhazlett22@AIR-WX1HA.local>
2018-10-29 14:29:45 -0500

    Basic UI Done: Menu still needed

---
+++ Battle of the Browsers/Assets/Scripts/GoToMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+    
+        {
+        }
+    }
+}
