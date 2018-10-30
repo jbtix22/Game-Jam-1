c1d7419a966fd7fc24f5fe42272abbe666701bb0
Jackson Tix <jbtix22@blakeschool.org>
2018-10-30 13:41:34 -0500

    Revert "Merge branch 'Jackson'"
    This reverts commit b75e2c7d9530d72b48f77ae42a75c3e75ade954f, reversing
    changes made to 5484a5f9e8cad32509a5b4354579a834f4c669ed.

---
+++ Battle of the Browsers/Assets/Scripts/EnergySlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergySlider : MonoBehaviour
+{
+
+    public Slider Energy;
+    public float energy_left;
+    public bool sendCallBack;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Energy.value += .0075F;
+        energy_left = Energy.value;
+        //Slider.Set(energybar.value, sendCallBack);
+    }
+
+    //public void EnergyLeft(float energy_left, bool sendCallBack);
+}
